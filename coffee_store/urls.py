9d9e898e8e0d5e226ae18b88d4a8ad3515983630
julianbohm <julianbohm9@gmail.com>
2025-04-01 11:59:10 +0200

    create model products, and urls coffee_store

--- coffee_store/urls.py
+++ coffee_store/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from products import views as product_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', product_views.coffee_list, name='coffee_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Coffee
 
 # Create your views here.
+
+def coffee_list(request):
+    coffees = Coffee.objects.filter(available=True)
+    return render(request, 'products/coffee_list.html', {'coffees': coffees})
