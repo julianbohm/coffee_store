1a91706f3bd56e9d6cdd571f4c18e8008fe75b71
julianbohm <julianbohm9@gmail.com>
2025-04-04 07:03:37 +0200

    update cart for remuve and modify items

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.view_cart, name='view_cart'),
     path('add/<int:coffee_id>/', views.add_to_cart, name='add_to_cart'),
-    path('view/', views.view_cart, name='view_cart'),
+    path('update/<int:item_id>/', views.update_quantity, name='update_quantity'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Coffee
 from .models import CartItem
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def add_to_cart(request, coffee_id):
     coffee = get_object_or_404(Coffee, id=coffee_id)
     cart_item, created = CartItem.objects.get_or_create(user=request.user, coffee=coffee)
@@ -11,7 +13,26 @@ def add_to_cart(request, coffee_id):
     cart_item.save()
     return redirect('coffee_list')
 
+@login_required
 def view_cart(request):
     cart_items = CartItem.objects.filter(user=request.user)
     total = sum(item.get_total_price() for item in cart_items)
     return render(request, 'cart/cart.html', {'cart_items': cart_items, 'total': total})
+
+@login_required
+def update_quantity(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id, user=request.user)
+    if request.method == "POST":
+        new_qty = int(request.POST.get('quantity', 1))
+        if new_qty > 0:
+            item.quantity = new_qty
+            item.save()
+        else:
+            item.delete()
+    return redirect('view_cart')
+
+@login_required
+def remove_from_cart(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id, user=request.user)
+    item.delete()
+    return redirect('view_cart')
