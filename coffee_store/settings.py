f6e284a437cfbfc5daa80eccfcfe9734eb65743a
julianbohm <julianbohm9@gmail.com>
2025-04-08 07:54:39 +0200

    database_url

--- coffee_store/settings.py
+++ coffee_store/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'coffee_store.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600, ssl_require=True)
     }
 else: 
     DATABASES = {
